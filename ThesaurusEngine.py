878d048dc3d2359317f40d6e36dc5b2451fac287
giladgh <73537309+giladghgh@users.noreply.github.com>
2021-01-13 19:32:57 +0000

    Update ThesaurusEngine.py

--- ThesaurusEngine.py
+++ ThesaurusEngine.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Maximising phoneme distribution in my MEng dissertation abstract.
-# ### *Clunky, flashy, and somewhat trashy.*
 
-# 
-# 
+
 
 # ### Preamble
 
